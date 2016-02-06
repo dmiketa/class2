5e0f39c12199ac88574bcc8620d1423aa4ac2098
Dora Miketa <dm3515@nyu.edu>
2016-02-06 12:07:16 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+	  	var LilNum = 1;
+	$('#LilBub').click( function(){
+		LilNum=LilNum+1;
+	  $('#LilNum').text(LilNum);
+	  });
+	  
+	var GrumNum = 1;  
+	$('#Grumpy').click( function(){
+	 GrumNum = GrumNum +1;
+	 $('#GrumNum').text(GrumNum);
+	});
+
+	var ColNum = 1;
+	$('#Colonel').click( function(){
+	 ColNum = ColNum+1;
+	 $('#ColNum').text(ColNum);
+	});
+
+	var CatNum = 1;
+	$('#Stevens').click( function(){
+	 CatNum = CatNum + 1;
+	 $('#CatNum').text(CatNum);
+	});
+
+	var GarNum = 1;
+	$('#Garfield').click( function(){
+	 GarNum = GarNum +1;
+	 $('#GarNum').text(GarNum);
+	});
